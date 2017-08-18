a8bc658c1813c6fd96e70551eeae28124720e581
Tomislav Adamic <tomislav.adamic@gmail.com>
2017-08-18 17:22:55 +0200

    Removed Python 3.4 support.

--- setup.py
+++ setup.py
@@ -44,7 +44,6 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: CPython',
