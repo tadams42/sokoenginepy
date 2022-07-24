0512eba676797d0810aaca0b981c655e7cea288a
Tomislav Adamic <tomislav.adamic@gmail.com>
2022-07-24 12:46:32 +0200

    docs: new themes for docs (Furo and doxygen-awesome)

--- cmake/modules/libsokoengine_doxygen.cmake
+++ cmake/modules/libsokoengine_doxygen.cmake
@@ -44,15 +44,27 @@ if(DOXYGEN_FOUND)
     set(DOXYGEN_WARN_IF_UNDOCUMENTED "NO")
     set(DOXYGEN_RECURSIVE "YES")
 
+    set(DOXYGEN_VERBATIM_HEADERS "NO")
+    set(DOXYGEN_GENERATE_LATEX "NO")
+    set(DOXYGEN_MACRO_EXPANSION "YES")
+    set(DOXYGEN_EXPAND_ONLY_PREDEF "YES")
+    set(DOXYGEN_PREDEFINED
+        " __GNUC__=9 "
+        " LIBSOKOENGINE_DLL= "
+        " LIBSOKOENGINE_LOCAL= "
+        " LIBSOKOENGINE_API= "
+    )
+    set(DOXYGEN_COLLABORATION_GRAPH "NO")
+    set(DOXYGEN_GROUP_GRAPHS "NO")
+    set(DOXYGEN_INCLUDED_BY_GRAPH "NO")
+    set(DOXYGEN_DIRECTORY_GRAPH "NO")
     set(DOXYGEN_FILE_PATTERNS
         "*.hpp"
         "*.h"
     )
-
     set(DOXYGEN_EXCLUDE_PATTERNS
         "*/SOK_format_specification.h"
     )
-
     set(DOXYGEN_EXCLUDE_SYMBOLS
         "boost"
         "*BOOST_FILESYSTEM_*"
@@ -61,45 +73,41 @@ if(DOXYGEN_FOUND)
         "snapshot_parsing"
         "benchmarks"
     )
-
     set(DOXYGEN_EXAMPLE_PATH
         "${PAGES_DIR}"
     )
-
     set(DOXYGEN_IMAGE_PATH
         "${CMAKE_SOURCE_DIR}/docs/images"
         "${CMAKE_SOURCE_DIR}/docs/skin_format"
     )
 
+    # HTML options and theming
+    set(USE_MDFILE_AS_MAINPAGE "${CMAKE_SOURCE_DIR}/README.md")
+
+    set(DOXYGEN_GENERATE_TREEVIEW "YES")
+    set(DOXYGEN_HTML_EXTRA_STYLESHEET
+        "${CMAKE_SOURCE_DIR}/docs/doxygen_addons/doxygen-awesome-css/doxygen-awesome.css"
+        "${CMAKE_SOURCE_DIR}/docs/doxygen_addons/doxygen-awesome-css/doxygen-awesome-sidebar-only.css"
+        "${CMAKE_SOURCE_DIR}/docs/doxygen_addons/doxygen-awesome-css/doxygen-awesome-sidebar-only-darkmode-toggle.css"
+    )
     set(
         DOXYGEN_HTML_EXTRA_FILES
         "${CMAKE_SOURCE_DIR}/docs/skin_format/YASC_Skin_Tutorial_1_02.pdf"
+        "${CMAKE_SOURCE_DIR}/docs/doxygen_addons/doxygen-awesome-css/doxygen-awesome-darkmode-toggle.js"
+        "${CMAKE_SOURCE_DIR}/docs/doxygen_addons/doxygen-awesome-css/doxygen-awesome-fragment-copy-button.js"
     )
+    set(DOXYGEN_HTML_HEADER "${CMAKE_SOURCE_DIR}/docs/doxygen_addons/header.html")
+    set(DOXYGEN_DOT_IMAGE_FORMAT "svg")
+    set(DOXYGEN_DOT_TRANSPARENT "YES")
 
-    set(DOXYGEN_VERBATIM_HEADERS "NO")
-    set(DOXYGEN_GENERATE_TREEVIEW "YES")
-    set(DOXYGEN_GENERATE_LATEX "NO")
-    set(DOXYGEN_MACRO_EXPANSION "YES")
-    set(DOXYGEN_EXPAND_ONLY_PREDEF "YES")
-    set(DOXYGEN_PREDEFINED
-        "__GNUC__=9"
-        "LIBSOKOENGINE_DLL="
-        "LIBSOKOENGINE_LOCAL="
-        "LIBSOKOENGINE_API="
-    )
-    set(DOXYGEN_COLLABORATION_GRAPH "NO")
-    set(DOXYGEN_GROUP_GRAPHS "NO")
-    set(DOXYGEN_INCLUDED_BY_GRAPH "NO")
-    set(DOXYGEN_DIRECTORY_GRAPH "NO")
-
+    # End HTML options and theming
     doxygen_add_docs(
         docs
-        "${CMAKE_SOURCE_DIR}/src/libsokoengine"
         "${CMAKE_SOURCE_DIR}/INSTALL.md"
-        "${CMAKE_SOURCE_DIR}/README.md"
         "${CMAKE_SOURCE_DIR}/docs/tutorial.md"
         "${CMAKE_SOURCE_DIR}/CHANGELOG.md"
         "${CMAKE_SOURCE_DIR}/docs/common_skins_format.md"
         "${PAGES_DOX}"
+        "${CMAKE_SOURCE_DIR}/src/libsokoengine"
     )
 endif(DOXYGEN_FOUND)
